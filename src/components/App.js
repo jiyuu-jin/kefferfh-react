d598bad688341d7afc864e72892f7f00fca4cc1b
jiyuu-jin <jiyuu.llc@gmail.com>
2017-10-26 23:41:32 -0400

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,35 @@ import ExampleComponent from './ExampleComponent';
 import {
     BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+const Header = () => ({
+  render() {
+    return (
+      <header className="header light-blue darken-4 z-depth-1">
+        <img alt="KefferFH" className="" src="/images/logo.png" />
+        <nav className="light-blue darken-4">
+          <div className="nav-wrapper">
+            <ul className="hide-on-med-and-down">
+              <li>
+                <Link to="/example">Price Lists</Link>
+              </li>
+            </ul>
+
+            <ul id="nav-mobile" className="side-nav" />
+            <a data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+          </div>
+        </nav>
+      </header>
+    );
+  },
+});
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <div id="page-content" className="thebackground">
-            <h1>Test Header</h1>
+            <Header />
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/example" component={ExampleComponent} />
