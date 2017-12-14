86e8e8cb1a2777e82de528aad4d836aec4a943d5
jiyuu-jin <jiyuu.llc@gmail.com>
2017-12-14 14:17:14 -0500

    Putting the final touches on

--- src/components/cards.js
+++ src/components/cards.js
@@ -27,13 +27,69 @@ const Cards = () => ({
         </div>
 
         <br />
+        <br />
+
+        <center>
+
+          <h5>Selection 2</h5>
+          <span>
+                Make me an instrument of thy peace;
+                Where there is hatred, let me sow love;<br />
+                Where there is injury, pardon.<br />
+                Where there is despair, hope;<br />
+                Where there is darkness, light;<br />
+                And where there is sadness, joy.
+          </span>
+
+          <br />
+          <br />
+
+          <h5>Selection 3</h5>
+
+          <span>
+                          O Divine Master, grant that I<br />
+            &nbsp;may not so much seek<br />
+            To be consoled...as to console,<br />
+            To be understood...as to understand,<br />
+            To be loved...as to love, for<br />
+            It is in giving...that we receive<br />
+            It is in pardoning, that we are pardoned<br />
+            It is in dying...that we are born to<br />
+            eternal life.<br />
+
+          </span>
+
+          <br />
+
+          <h5>Selection 4</h5>
+
+          <span>
+                              Lord, make me an instrument&nbsp;of your peace<br />
+            Where there is hatred...let me sow love<br />
+            Where there is injury...pardon.<br />
+            Where there is doubt...faith.<br />
+            Where there is despair...hope.<br />
+            Where there is darkness...light.<br />
+            Where there is sadness...joy.<br />
+            O Divine Master, grant that I may not&nbsp;so much seek<br />
+            To be consoled...as to console,<br />
+            To be understood...as to understand,<br />
+            To be loved...as to love, for<br />
+            It is in giving...that we receive<br />
+            It is in pardoning, that we are pardoned<br />
+            It is in dying...that we are born to&nbsp;eternal life.
+              </span>
+        </center>
+
+
+        <br />
 
       </div>
     );
   },
 
   componentDidMount() {
-      $('.slider').slider();
+    $('.slider').slider();
   },
 });
 
