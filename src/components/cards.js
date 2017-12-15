279d49a9191805dc0ffcbba4e3abfdffe87d0b08
jiyuu-jin <jiyuu.llc@gmail.com>
2017-12-14 21:39:17 -0500

    Meta fixes

--- src/components/cards.js
+++ src/components/cards.js
@@ -79,6 +79,150 @@ const Cards = () => ({
             It is in pardoning, that we are pardoned<br />
             It is in dying...that we are born to&nbsp;eternal life.
               </span>
+
+
+          <br />
+
+          <h5>Selection 5</h5>
+
+          <span>
+            May the road rise to meet you, <br />
+
+            May the wind be always to your back. <br />
+
+            May the sun shine warm upon your face, <br />
+
+            May the rains fall soft upon your fields <br />
+
+            and until we meet again, <br />
+
+            May God hold you in the palm of his hand <br />
+          </span>
+
+
+          <br />
+
+          <h5>Selection 6</h5>
+
+
+          <span>
+                            Footprints <br />
+              One night a man had a dream. He dreamed he was walking along the beach with the LORD. <br />
+
+            Across the sky flashed scenes from his life. <br />
+
+            For each scene, he noticed two sets of footprints in the sand; one belonging to him and the other to the LORD. <br />
+              When the last scene of his life flashed before him, he looked back at the footprints in the sand.
+
+            He noticed that many times along the path of his life there was only one set of footprints. <br />
+
+            He also noticed that it happened at the very lowest and saddest times in his life. <br />
+              This really bothered him and he questioned the LORD about it. <br />
+
+            "LORD, you said that once I decided to follow you, you'd walk with me all the way. But I have noticed that during the most troublesome times in my life, there is only one set of footprints. I don't understand why when I needed you most you would leave me." <br />
+              The LORD replied, "My precious child, I love you and I would never leave you. During your times of trial and suffering, when you see only one set of footprints, it was then that I carried you." <br />
+          </span>
+
+          <br />
+
+          <h5>Selection 7</h5>
+
+          <span>
+                        23rd  PSALM
+        The Lord is my shepherd, I shall not want. <br />
+        He maketh me lie down in green pastures; <br />
+        He leadeth me beside the still waters. <br />
+        He restoreth my soul; He leadeth me inthe paths of righteousness for his name's sake.
+        Yea,  though I walk  through  the valley of  the  shadow of  death I  will fear no evil for  thou art with me; thy rod and thy staff they comfort me. <br />
+
+        Thou preparest a table before me in the presence of mine enemies; thou annointest my head with oil; my cup runneth over.                                                                                             <br />
+        Surely goodness and mercy shall  follow me all the days of my life; and I will  dwell in the house of the  Lord <br />
+          </span>
+
+          <br />
+
+          <h5>Selection 8</h5>
+
+          <span>
+                "Yo soy la Resurreccion y la Vida; el que cree en <br />
+                Mi aunque este muerto vivira y todo aquel que <br />
+                vive y cree en Mi no morira eternamente." <br />
+
+          </span>
+
+          <br />
+          <br />
+
+          <h6>*The following are available on Memorial Folders only:</h6>
+
+
+          <h5>Selection 9</h5>
+          <span>
+            I'M FREE <br />
+            Don't grieve for me, for now I'm free. <br />
+            I'm following the path God laid for me. <br />
+            I took His hand when I heard Him call, <br />
+            turned my back and left it all. <br />
+
+            <br />
+
+            I could not stay another day. <br />
+            To laugh, to love, to work or play. <br />
+            Tasks left undone must stay that  way. <br />
+            I've found that peace at close of day. <br />
+
+            <br />
+
+            If my parting has left a void, , <br />
+            Then fill it with remembered joy. <br />
+            A friendship shared, a laugh, a kiss. <br />
+            Ah, yes, these things too, I will miss. <br />
+
+            <br />
+
+            Be not burdened with time of sorrow, <br />
+            I wish you the sunshine of tomorrow. <br />
+            My life's been full, I savored much. <br />
+            Good friends, good times, a loved ones' touch. <br />
+
+            <br />
+
+            Perhaps my time seemed all too brief, <br />
+            Don't lengthen it now with undue grief. <br />
+            Lift up your heart and share with me. <br />
+                God wanted me now; He set me free. <br />
+
+          </span>
+
+
+            <h5>Selection 10</h5>
+
+            <span>
+                MISS ME - BUT LET ME GO <br />
+                When I come to the end of the road <br />
+                and the sun has set for me,
+                I want no rites in a gloom filled room <br />
+                Why cry for a soul  set free? <br />
+
+                Miss me a little, but not for long <br />
+                And not with your head bowed low. <br />
+                Remember the love that we once shared. <br />
+                Miss me - but let  me go. <br />
+
+                For this is a journey <br />
+                That we all must take <br />
+                And each must go alone. <br />
+                It's all a part of the master's plan. <br />
+                A step on the road to home, <br />
+
+                When you are lonely and sick of heart, <br />
+                Go to the friends we know <br />
+                And bury your sorrows <br />
+                In doing good deeds. <br />
+                Miss me - but let me go. <br />
+
+            </span>
+
         </center>
 
 
