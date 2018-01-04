58849c65e79e052ac38337a47ca016c8651fcf7f
jiyuu-jin <jiyuu.llc@gmail.com>
2018-01-04 14:34:17 -0500

    Success

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,7 +55,7 @@ class Home extends Component {
                 <div className="card z-depth-5">
                   <a className="black-text" href="https://www.google.com/maps/dir//John+W+Keffer+Funeral+Home+Inc,+902+Mt+Rose+Ave,+York,+PA+17403/@39.9609965,-76.7092943,17z/data=!4m15!1m6!3m5!1s0x89c88ec6f9dd0131:0xce3ff1ada7892b06!2sJohn+W+Keffer+Funeral+Home+Inc!8m2!3d39.9609965!4d-76.7071056!4m7!1m0!1m5!1m1!1s0x89c88ec6f9dd0131:0xce3ff1ada7892b06!2m2!1d-76.7071056!2d39.9609965">
                     <div className="card-image">
-                      <img alt=" " src="https://static.wixstatic.com/media/09384e_602a537842194aef9352d88498512864.jpg/v1/fill/w_200,h_120,al_c,q_80/09384e_602a537842194aef9352d88498512864.webp" />
+                      <img alt=" " src="/images/east_york.jpg" />
                     </div>
                     <h5 className="card-title center">Keffer Funeral Home</h5>
                     <div className="card-content center">
@@ -72,7 +72,7 @@ class Home extends Component {
                 <div className="card z-depth-5">
                   <a className="black-text" href="https://www.google.com/maps/dir/''/371+Kings+Mill+Rd,+York,+PA+17401/data=!4m5!4m4!1m0!1m2!1m1!1s0x89c88eaa91b286f7:0xe8c44fa381f4efc6?sa=X&ved=0ahUKEwjao7CZwcTVAhVLMSYKHdUFAOUQwwUIKTAA">
                     <div className="card-image">
-                      <img alt=" " src="https://static.wixstatic.com/media/09384e_b9814df22f8f47d9b38e0f1336f647fd.jpg/v1/fill/w_200,h_120,al_c,q_80/09384e_b9814df22f8f47d9b38e0f1336f647fd.webp" />
+                      <img alt=" " src="/images/crematory.jpg" />
                     </div>
                     <h5 className="card-title center">Cremation Direct Service of York Crematory</h5>
                     <div className="card-content center">
@@ -88,7 +88,7 @@ class Home extends Component {
                 <div className="card z-depth-5">
                   <a className="black-text" href="https://www.google.com/maps/dir//2114+W+Market+St,+York,+PA+17404/@39.9518766,-76.7752103,17z/data=!4m16!1m7!3m6!1s0x89c88b86be60ea29:0x91b62406d9c1360a!2s2114+W+Market+St,+York,+PA+17404!3b1!8m2!3d39.9518766!4d-76.7730216!4m7!1m0!1m5!1m1!1s0x89c88b86be60ea29:0x91b62406d9c1360a!2m2!1d-76.7730216!2d39.9518766">
                     <div className="card-image">
-                      <img alt=" " src="https://static.wixstatic.com/media/09384e_c58db175f4144e82a0ab6fe718666dfa.jpg/v1/fill/w_200,h_120,al_c,q_80/09384e_c58db175f4144e82a0ab6fe718666dfa.webp" />
+                      <img alt=" " src="/images/west_york.jpg" />
                     </div>
                     <h5 className="card-title center">Keffer Funeral Home</h5>
                     <div className="card-content center">
@@ -99,82 +99,6 @@ class Home extends Component {
                     </div>
                   </a>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="contact" className="parallax-container valign-wrapper">
-          <div className="section no-pad-bot">
-            <div className="container">
-              <div className="card card-up">
-                <div className="card-content">
-                  <div className="row">
-                    <form className="col s12">
-                      <div className="row">
-                        <div className="input-field col s6">
-                          <input id="first_name" type="text" className="validate" />
-                          <label htmlFor="first_name">First Name</label>
-                        </div>
-                        <div className="input-field col s6">
-                          <input id="last_name" type="text" className="validate" />
-                          <label htmlFor="last_name">Last Name</label>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="input-field col s12">
-                          <input id="email" type="email" className="validate" />
-                          <label htmlFor="email">Email</label>
-                        </div>
-                      </div>
-                      <center>
-                      <button className="btn waves-effect waves-light" type="submit" name="action">
-                        <i className="material-icons right">send</i>
-                      </button>
-                      </center>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="parallax"><img src="/images/background2.jpg" alt="Unsplashed background img 2" /></div>
-        </div>
-
-
-        <div className="container">
-          <div className="section">
-            <div className="row">
-              <div className="col s12 m3">
-                <Link className="black-text" to="/caskets">
-                  <div className="icon-block">
-                    <h5>Caskets</h5>
-                    <img alt=" " src="/images/casket.webp" />
-                  </div>
-                </Link>
-              </div>
-              <div className="col s12 m3">
-                <Link className="black-text" to="/containers">
-                  <div className="icon-block">
-                    <h5>Burial Containers</h5>
-                    <img alt=" " src="/images/burial_container.webp" />
-                  </div>
-                </Link>
-              </div>
-              <div className="col s12 m3">
-                <a className="black-text" href="http://www.pageturnpro.com/Crescent-Memorial/47278-Crescent-Memorial/puredefault.html">
-                  <div className="icon-block">
-                    <h5>Urns</h5>
-                    <img alt=" " height="170px" src="/images/urn.png" />
-                  </div>
-                </a>
-              </div>
-              <div className="col s12 m3">
-                <Link className="black-text" to="/cards">
-                  <div className="icon-block">
-                    <h5>Prayer Cards</h5>
-                    <img alt=" " height="170px" src="/images/prayer_card.webp" />
-                  </div>
-                </Link>
               </div>
             </div>
           </div>
@@ -234,6 +158,83 @@ class Home extends Component {
             </div>
           </div>
         </div>
+
+        <div className="container">
+          <div className="section">
+            <div className="row">
+              <div className="col s12 m3">
+                <Link className="black-text" to="/caskets">
+                  <div className="icon-block">
+                    <h5>Caskets</h5>
+                    <img alt=" " src="/images/casket.webp" />
+                  </div>
+                </Link>
+              </div>
+              <div className="col s12 m3">
+                <Link className="black-text" to="/containers">
+                  <div className="icon-block">
+                    <h5>Burial Containers</h5>
+                    <img alt=" " src="/images/burial_container.webp" />
+                  </div>
+                </Link>
+              </div>
+              <div className="col s12 m3">
+                <a className="black-text" href="http://www.pageturnpro.com/Crescent-Memorial/47278-Crescent-Memorial/puredefault.html">
+                  <div className="icon-block">
+                    <h5>Urns</h5>
+                    <img alt=" " height="170px" src="/images/urn.png" />
+                  </div>
+                </a>
+              </div>
+              <div className="col s12 m3">
+                <Link className="black-text" to="/cards">
+                  <div className="icon-block">
+                    <h5>Prayer Cards</h5>
+                    <img alt=" " height="170px" src="/images/prayer_card.webp" />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="contact" className="parallax-container valign-wrapper">
+          <div className="section no-pad-bot">
+            <div className="container">
+              <div className="card card-up">
+                <div className="card-content">
+                  <div className="row">
+                    <form className="col s12">
+                      <div className="row">
+                        <div className="input-field col s6">
+                          <input id="first_name" type="text" className="validate" />
+                          <label htmlFor="first_name">First Name</label>
+                        </div>
+                        <div className="input-field col s6">
+                          <input id="last_name" type="text" className="validate" />
+                          <label htmlFor="last_name">Last Name</label>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="input-field col s12">
+                          <input id="email" type="email" className="validate" />
+                          <label htmlFor="email">Email</label>
+                        </div>
+                      </div>
+                      <center>
+                        <button className="btn waves-effect waves-light" type="submit" name="action">
+                          <i className="material-icons right">send</i>
+                        </button>
+                      </center>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="parallax"><img src="/images/background2.jpg" alt="Unsplashed background img 2" /></div>
+        </div>
+
 
 
         <div className="container center">
